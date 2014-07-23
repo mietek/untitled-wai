616b7dfeb85c4d4e86702d81461a63eaa61e4805
Miëtek Bak <mietek@bak.io>
2014-07-23 23:45:34 +0100

    Frob bits

--- src/Main.hs
+++ src/Main.hs
@@ -8,7 +8,7 @@ import Control.Applicative ((<$>))
 import System.Environment (getEnv)
 import System.Exit (ExitCode (..))
 import System.IO (BufferMode (..), hSetBuffering, stdout)
-import System.Posix.Process (exitImmediately, getProcessID)
+import System.Posix.Process (exitImmediately)
 import System.Posix.Signals (Handler (..), installHandler, sigTERM)
 
 import qualified Network.HTTP.Types as HTTP
@@ -23,21 +23,19 @@ import DB (initDB)
 main :: IO ()
 main = do
     hSetBuffering stdout NoBuffering
-    _ <- installHandler sigTERM (Catch handleSIGTERM) Nothing
     port <- read <$> getEnv "PORT"
+    _ <- installHandler sigTERM (Catch (handleSIGTERM port)) Nothing
     dburl <- getEnv "DATABASE_URL"
     db <- initDB dburl
     _ <- initAuth db
-    pid <- getProcessID
-    putStrLn (show pid ++ ": Started on port " ++ show port)
+    putStrLn ("Listening started on port " ++ show port)
     Warp.run port app
 
 --------------------------------------------------------------------------------
 
-handleSIGTERM :: IO ()
-handleSIGTERM = do
-    pid <- getProcessID
-    putStrLn (show pid ++ ": Stopped with SIGTERM")
+handleSIGTERM :: Int -> IO ()
+handleSIGTERM port = do
+    putStrLn ("Listening stopped on port " ++ show port)
     exitImmediately ExitSuccess
 
 app :: WAI.Application
